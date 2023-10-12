3818cb7443fb1d30a95486dfa57b59008710707d
Uche Ogbonna <ucheogbonnak@gmail.com>
2023-10-12 02:30:15 +0100

    added all

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,70 @@
+import "./contact.css";
+import Phone from "../../img/phone.jpg";
+import Email from "../../img/email.jpg";
+import Address from "../../img/address.jpg";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+  const formRef = useRef();
+  const [done, setDone] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_da8l54b",
+        "template_an11c0e",
+        formRef.current,
+        "B6Xx09ss0UgI7hu7M"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Lets discuss your project</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              +234 9068 973 430
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Email} alt="" />
+              ucheogbonnak@gmail.com
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Address} alt="" />
+              245 King Street, Touterie Victoria 8520 Australia
+            </div>
+          </div>
+        </div>
+        <div className="c-right">
+          <p className="c-desc">
+            <b>What's your story?</b> Get in touch. Always available for
+            freelancing if the right project comes along. me.
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" name="user_name" />
+            <input type="text" placeholder="Subject" name="user_subject" />
+            <input type="text" placeholder="Email" name="user_email" />
+            <textarea name="message" rows="5" placeholder="message" />
+            <button>Submit</button>
+            {done && "Thank you ..."}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://i.pinimg.com/564x/4c/73/83/4c73832a83589553ea3c5a71e9fc2480.jpg",
+    link: "http://lama.dev",
+  },
+  {
+    id: 2,
+    img: "https://i.pinimg.com/564x/89/8d/35/898d35ccd797899c38b95a85157a6bc8.jpg",
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: "https://i.pinimg.com/564x/85/72/cc/8572cca7244b259557753255c23ffe87.jpg",
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: "https://i.pinimg.com/564x/6a/2a/34/6a2a34b2afc0fd57dc33d316f437303e.jpg",
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: "https://i.pinimg.com/564x/26/6f/35/266f35cb3e07daa453a66bf095cedce3.jpg",
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: "https://i.pinimg.com/564x/99/e8/09/99e809f8b751b68f09aa7ae771847c68.jpg",
+    link: "http://lama.dev",
+  },
+];
